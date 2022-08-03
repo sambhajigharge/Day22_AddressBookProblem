69e1fdf1978e7898a1684b99d6aae4e6957c11fd
sushant <sgharge299@gmail.com>
2022-08-03 13:00:35 +0530

    [sushant]added the create multiple contact method

--- AddressBook.java
+++ AddressBook.java
@@ -5,6 +5,14 @@ public class AddressBook {
         System.out.println("Welcome to Address book Program");
         Utility utility = new Utility();
         utility.contactDetails();
+
+        Utility utility1 = new Utility();
+        System.out.println("Add Second contact details \n");
+        utility1.contactDetails();
+
+        Utility utility2 = new Utility();
+        System.out.println("Add Third contact details \n");
+        utility2.contactDetails();
     }
 }
 
